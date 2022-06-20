394c864d391e15140396aaebff3e9a6c17178b32
yiannisha <hadjiyiannis.3@gmail.com>
2022-06-20 23:42:14 +0300

    Added some tests for the scraper module

--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -9,6 +9,17 @@ from __main__ import scraper
 class ScraperTest (unittest.TestCase):
     """ A class to test the Scraper class """
 
+    def setUp (self) -> None:
+        """ Sets up a Scraper with default country and currency for the tests. """
+        self.scraper = scraper.Scraper()
+
     def test_TestClassWorking (self) -> None:
         """ Simple test that the test class is working """
         self.assertEqual(1, 1)
+
+    def test_ScraperSetUp (self) -> None:
+        pass
+
+    def tearDown (self) -> None:
+        """ Closes all drivers opened due to tests. """
+        pass

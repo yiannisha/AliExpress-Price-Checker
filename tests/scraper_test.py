8f55e8dc2b0b162a0cfc56f2d7b40445beddd14d
yiannisha <hadjiyiannis.3@gmail.com>
2022-06-22 20:48:33 +0300

    Added simple Scraper setup test

--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -11,15 +11,20 @@ class ScraperTest (unittest.TestCase):
 
     def setUp (self) -> None:
         """ Sets up a Scraper with default country and currency for the tests. """
-        self.scraper = scraper.Scraper()
+        pass
+        # self.scraper = scraper.Scraper()
 
     def test_TestClassWorking (self) -> None:
         """ Simple test that the test class is working """
         self.assertEqual(1, 1)
 
     def test_ScraperSetUp (self) -> None:
-        pass
+        self.scraper = scraper.Scraper(country = 'china')
 
     def tearDown (self) -> None:
         """ Closes all drivers opened due to tests. """
-        pass
+        try:
+            self.scraper.close()
+        except Exception as e:
+            print(e)
+            pass

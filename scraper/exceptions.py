7e016ed49cbc926fd45477b15bf6b4e80de4daad
yiannisha <hadjiyiannis.3@gmail.com>
2022-06-22 21:37:13 +0300

    Added exception for errors in navigation using classes

--- scraper/exceptions.py
+++ scraper/exceptions.py
@@ -4,7 +4,21 @@
 
 class InvalidCountryException(Exception):
     """ Raised when an invalid country is passed as an argument to a Scraper. """
-    pass
+    def __init__(self, country: str, message: str = None) -> None:
+        self.country = country
+        if not message:
+            message = f'Cannot find country {country}.'
+        self.message = message
+        super().__init__(self.message)
+
+class InvalidCurrencyException(Exception):
+    """ Raised when an invalid currency is passed as an argument to a Scraper. """
+    def __init__(self, currency: str, message: str = None) -> None:
+        self.currency = currency
+        if not message:
+            message = f'Cannot find currency {currency}.'
+        self.message = message
+        super().__init__(self.message)
 
 class InvalidXpathNavigationException(Exception):
     """
@@ -21,6 +35,28 @@ class InvalidXpathNavigationException(Exception):
         self.elementName = elementName
         if not message:
             message = f'Element with supposed xpath: {xpath}'
+            if elementName:
+                message += f' with the name of {elementName}'
+            message += ' cannot be found.'
+        self.message = message
+
+        super().__init__(self.message)
+
+class InvalidClassNameNavigationException(Exception):
+    """
+    Raised when there an element can't be found.
+    Raised only when we search for the element by class name.
+
+    Attributes:
+        className : string of the class name that the search was made with
+        elementName : optional name passed for the searched element
+    """
+    def __init__(self, className: str, message: str = None, elementName: str = None) -> None:
+
+        self.className = className
+        self.elementName = elementName
+        if not message:
+            message = f'Element with supposed className: {className}'
             if elementName:
                 message += f' with the name of {elementName}'
             message += ' cannot be found.'

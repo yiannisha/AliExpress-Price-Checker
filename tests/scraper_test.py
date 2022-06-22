cf366c6ef46579dc3803c6033972f2d67ef552bd
yiannisha <hadjiyiannis.3@gmail.com>
2022-06-22 22:14:55 +0300

    Added tests for headless mode

--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -2,9 +2,18 @@
 
 """ Tests for the scraper module """
 
+# stdlib modules
 import unittest
 
+# thrid-party modules
+from selenium.webdriver.common.by import By
+
+# module to be tested
 from __main__ import scraper
+
+# exception modules
+from __main__ import exceptions
+from selenium.common.exceptions import NoSuchElementException
 
 class ScraperTest (unittest.TestCase):
     """ A class to test the Scraper class """
@@ -18,8 +27,44 @@ class ScraperTest (unittest.TestCase):
         """ Simple test that the test class is working """
         self.assertEqual(1, 1)
 
-    def test_ScraperSetUp (self) -> None:
-        self.scraper = scraper.Scraper(country = 'china', currency = 'usd')
+    def test_ScraperWindowedSetUp (self) -> None:
+        """ Tests that the debug windowed Scraper set up is working as intended """
+        scr = scraper.Scraper(country='china', currency='usd', headless=False)
+        scr.close()
+
+    # @unittest.skip
+    def test_ScraperHeadlessSetUp (self) -> None:
+        """ Tests that the headless Scraper set up is working as intended """
+        self.scraper = scraper.Scraper(country='china', currency='usd', headless=True)
+
+        # debug
+        with open('debug.html', 'w', encoding='utf-8') as f:
+            f.write(self.scraper.driver.page_source)
+
+        # verify that save button is clicked
+        self.scraper.driver.refresh()
+
+        flag_xpath = '//*[@id="switcher-info"]/span[1]/i'
+        try:
+            flag = self.scraper.driver.find_element(By.XPATH, flag_xpath)
+        except NoSuchElementException:
+            raise exceptions.InvalidXpathNavigationException(xpath=flag_xpath, elementName='settings menu flag')
+
+        currency_xpath = '//*[@id="switcher-info"]/span[5]'
+        try:
+            currency = self.scraper.driver.find_element(By.XPATH, currency_xpath)
+        except NoSuchElementException:
+            raise exceptions.InvalidXpathNavigationException(xpath=currency_xpath, elementName='settings menu currency')
+
+        expected_values = {
+            'flag': 'css_flag css_hk',
+            'currency': 'USD'
+        }
+
+        self.assertEqual(flag.get_attribute('class'), expected_values['flag'])
+        self.assertEqual(currency.text, expected_values['currency'])
+
+
 
     def tearDown (self) -> None:
         """ Closes all drivers opened due to tests. """

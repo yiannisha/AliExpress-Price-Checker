44800b0b4e49584630f227a7574cf38d0a8954d3
yiannisha <hadjiyiannis.3@gmail.com>
2022-06-24 15:01:33 +0300

    Added tests for scraper's new functionality

--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -15,17 +15,35 @@ from __main__ import scraper
 from __main__ import exceptions
 from selenium.common.exceptions import NoSuchElementException
 
+# typing
+from typing import Tuple
+
 class ScraperTest(unittest.TestCase):
     """ A class to test the Scraper class. """
 
     def setUp (self) -> None:
         """ Sets up a windowed Scraper to be used later. """
-        self.scraper = scraper.Scraper(country='china', currency='usd', headless=False)
+        self.scraper = scraper.Scraper(country='united kingdom', currency='usd', headless=False)
 
     def test_scrapeURL (self) -> None:
         """ Tests the Scraper.scrapeURL method. """
-        self.scraper.scrapeURL('url')
+
+        expected_values = [
+            ('https://www.aliexpress.com/item/1005003742432861.html', True, 24.69, 0),
+            ('https://www.aliexpress.com/item/1005004285231560.html', False, 11.54, 0),
+            ('https://www.aliexpress.com/item/1005003890863335.html', False, 17.54, 0),
+            ('https://www.aliexpress.com/item/1005004047047021.html', True, 14.61, 0),
+            ('https://www.aliexpress.com/item/1005003604897865.html', True, 4.69, 2.63)
+        ]
+
+        for url, tracking, itemPrice, shipPrice in expected_values:
+            data: Tuple[float, float]
+            data = self.scraper.scrapeURL(url, tracking)
+            self.assertEqual(data[0], itemPrice)
+            self.assertEqual(data[1], shipPrice)
 
     def tearDown (self) -> None:
         """ Closes running Scrapers """
+        with open('debug.html', 'w', encoding='utf-8') as f:
+            f.write(self.scraper.driver.page_source)
         self.scraper.close()

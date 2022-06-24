7d8db8a0b01d5a8a713344eafb56872731fd0db2
yiannisha <hadjiyiannis.3@gmail.com>
2022-06-24 15:01:06 +0300

    Added item and shipping price scraping

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,13 +6,13 @@
 import os
 import re
 import sys
-import platform
+import time
 
 # third party modules
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # inner modules
 from scraper import driver
@@ -24,12 +24,7 @@ from selenium.common.exceptions import ElementNotInteractableException
 from selenium.common.exceptions import ElementClickInterceptedException
 
 # typing
-from typing import Union
-
-# typedef
-ChromeWebdriver: webdriver.chrome.webdriver.WebDriver
-ChromeWebdriver = webdriver.chrome.webdriver.WebDriver
-
+from typing import Tuple
 
 class Scraper(driver.Driver):
     """
@@ -41,3 +36,151 @@ class Scraper(driver.Driver):
 
     def __init__ (self, country: str = None, currency: str = None, headless: bool = True) -> None:
         super().__init__(country, currency, headless)
+
+    def scrapeURL (self, url: str, tracking: bool) -> Tuple[float, float]:
+        """
+        Scrapes the given AliExpress url for the item price and the shipping price.
+
+        :param url: item page url
+        :param tracking: whether or not to get the cheapest tracking option in shipping
+        """
+
+        self.driver.get(url)
+
+        # select all first options (color, size etc.)
+        # TODO
+
+        # get item price string
+        itemPriceString = self.getItemPriceString(url).replace(',', '.')
+        # and convert it to a float
+        itemPrice = self.convertPriceToFloat(itemPriceString)
+
+        # get the shipping price string
+        # firstly validate that tracking is available if tracking is true
+        if tracking:
+            self.setShippingTracking()
+        shippingPriceString = self.getShippingPriceString(url).replace(',', '.')
+        if re.search('Free Shipping', shippingPriceString):
+            shippingPrice = 0
+        else:
+            shippingPrice = self.convertPriceToFloat(shippingPriceString)
+
+        return (itemPrice, shippingPrice)
+
+    def getItemPriceString (self, url: str) -> str:
+        """
+        Scrapes the item price from an item page by trying to get the price from
+        different possible elements.
+        Assumes that driver is already at the page.
+        Raises ItemPriceNotFoundException if string to be returned is empty.
+
+        :param url: needed for ItemPriceNotFoundException to be raised
+        """
+
+        possible_classes = [
+            'product-price-current',
+            'uniform-banner-box-price',
+        ]
+
+        itemPriceString = ''
+
+        for className in possible_classes:
+            try:
+                price_elem = self.driver.find_element(By.CLASS_NAME, className)
+                itemPriceString = price_elem.text
+                break
+            except NoSuchElementException:
+                sys.stderr.write(f'No element with class {className}, trying next one...\n')
+
+        if not itemPriceString:
+            raise ItemPriceNotFoundException(url=url, classes=possible_classes)
+
+        return itemPriceString
+
+    def getShippingPriceString (self, url: str) -> str:
+        """
+        Scrapes the shipping price from an item page by trying to get the price from
+        different possible elements.
+        Assumes that driver is already at the page.
+        Raises ShippingPriceNotFoundException if string to be returned is empty.
+
+        :param url: needed for ShippingPriceNotFoundException to be raised
+        """
+
+        possible_classes = [
+            # '//*[@id="root"]/div/div[2]/div/div[2]/div[12]/div/div/div[1]/span/span/strong',
+            'dynamic-shipping',
+        ]
+
+        shippingPriceString = ''
+
+        for className in possible_classes:
+            try:
+                price_elem = self.driver.find_element(By.CLASS_NAME, className)
+                shippingPriceString = price_elem.text
+                break
+            except NoSuchElementException:
+                sys.stderr.write(f'No element with class {className}, trying next one...\n')
+
+        if not shippingPriceString:
+            raise ShippingPriceNotFoundException(url=url, classes=possible_classes)
+
+        return shippingPriceString
+
+    def setShippingTracking (self) -> None:
+        """
+        Sets the shipping option to the cheapest tracking option.
+        """
+
+        # press button to open shipping options
+        buttonClassName = 'comet-btn'
+        try:
+            elem = self.driver.find_element(By.CLASS_NAME, buttonClassName)
+            elem.click()
+        except NoSuchElementException:
+            raise InvalidClassNameNavigationException(className=buttonClassName, elementName='shipping options button')
+
+        # explicitly wait for the list to open by checking for list elements
+        listElementClass = 'dynamic-shipping-mark'
+        try:
+            WebDriverWait(self.driver, 3).until(
+                EC.presence_of_element_located((By.CLASS_NAME, listElementClass))
+            )
+        except NoSuchElementException:
+            raise InvalidClassNameNavigationException(className=listElementClass, elementName='shipping options list element')
+
+        # press button for more options if available
+        try:
+            elems = self.driver.find_elements(By.CLASS_NAME, buttonClassName)
+            if len(elems) > 1:
+                elems[1].click()
+        except NoSuchElementException:
+            sys.stderr.write('No "More options" button.\n')
+
+        # get first element with tracking available (already sorted from cheapest to most expensive)
+        trackingClassName = 'dynamic-shipping-mark'
+        try:
+            shippingOptions = self.driver.find_elements(By.CLASS_NAME, trackingClassName)
+        except NoSuchElementException:
+            raise InvalidClassNameNavigationException(className=trackingClassName, elementName='shipping options list element')
+
+        found = False
+        for option in shippingOptions:
+            if option.text == 'Tracking Available':
+                found = True
+                option.click()
+                break
+
+        # there are no tracking available options so just click the first option to close the list
+        if not found:
+            shippingOptions[0].click()
+
+    def convertPriceToFloat (self, price: str) -> float:
+        """
+        Converts a string with a price to a float value. (the price)
+
+        :param price: the price string
+        """
+
+        pattern = '([\d.,]+)'
+        return float(re.search(pattern, price).groups()[0])

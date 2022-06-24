5c1a3af4aa077ae947b296677eb5da05f8633e9a
yiannisha <hadjiyiannis.3@gmail.com>
2022-06-24 14:59:43 +0300

    Added exceptions for shipping and item prices

--- scraper/exceptions.py
+++ scraper/exceptions.py
@@ -2,8 +2,11 @@
 
 """ All exceptions raised by the Scraper class """
 
+# typing
+from typing import List
+
 class InvalidCountryException(Exception):
-    """ Raised when an invalid country is passed as an argument to a Scraper. """
+    """ Raised when an invalid country is passed as an argument to a Driver. """
     def __init__(self, country: str, message: str = None) -> None:
         self.country = country
         if not message:
@@ -12,7 +15,7 @@ class InvalidCountryException(Exception):
         super().__init__(self.message)
 
 class InvalidCurrencyException(Exception):
-    """ Raised when an invalid currency is passed as an argument to a Scraper. """
+    """ Raised when an invalid currency is passed as an argument to a Driver. """
     def __init__(self, currency: str, message: str = None) -> None:
         self.currency = currency
         if not message:
@@ -62,4 +65,50 @@ class InvalidClassNameNavigationException(Exception):
             message += ' cannot be found.'
         self.message = message
 
+        super().__init__(self.message)
+
+class ItemPriceNotFoundException(Exception):
+    """
+    Raised when the Scraper can't find an item's price.
+
+    Attributes:
+        url : item page's url
+        classes : list of classes tried to find the item price
+        xpaths : list of xpaths tried to find the item price
+    """
+    def __init__(self, url: str, message: str = None, classes: List[str] = [], xpaths: List[str] = []) -> None:
+
+        self.url = url
+        self.classes = classes
+        self.xpaths = xpaths
+        if not message:
+            message = f'Item price at {url} cannot be found.'
+            if classes:
+                message += f'\nClasses tried: {classes}'
+            if xpaths:
+                message += f'\nXpaths tried: {xpaths}'
+        self.message = message
+        super().__init__(self.message)
+
+class ShippingPriceNotFoundException(Exception):
+    """
+    Raised when the Scraper can't find an item's shipping price.
+
+    Attributes:
+        url : item page's url
+        classes : list of classes tried to find the shipping price
+        xpaths : list of xpaths tried to find the shipping price
+    """
+    def __init__(self, url: str, message: str = None, classes: List[str] = [], xpaths: List[str] = []) -> None:
+
+        self.url = url
+        self.classes = classes
+        self.xpaths = xpaths
+        if not message:
+            message = f'Item shipping price at {url} cannot be found.'
+            if classes:
+                message += f'\nClasses tried: {classes}'
+            if xpaths:
+                message += f'\nXpaths tried: {xpaths}'
+        self.message = message
         super().__init__(self.message)
